b6820b771e74f8f8af4506dc74dc206889d3c5ba
Faizan <53142548+faizan517@users.noreply.github.com>
2023-02-28 02:09:53 +0500

    Delete assign11 directory

--- assign11/js/app.js
+++
@@ -1,6 +0,0 @@
-var rightNow = new Date();
-// console.log("🚀 ~ file: app.js ~ line 2 ~ rightNow", rightNow)
- 
-var theDay = rightNow.getDay();
-console.log("🚀 ~ file: app.js ~ line 5 ~ theDay", theDay)
-
